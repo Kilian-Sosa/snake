88040bd067e7e6f7523d16fef2c3aa723b27ccd3
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-23 18:59:57 +0100

    Squashed commit of the following:
    commit ec6196ba0a2f3db49875dbf3d3c4617ee25ca947
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 18:59:44 2023 +0100
    
        Squashed commit of the following:
    
        commit 13df904091c9bf328503a98693db8797e1bd2cad
        Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
        Date:   Mon Oct 23 18:59:08 2023 +0100
    
            feat: Show score all the time. Also change layout
    
        commit a77845e724ab711ed251598e509096400a0bfd17
        Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
        Date:   Mon Oct 23 17:09:10 2023 +0100
    
            feat: Add timer
    
        commit 2ad0eeb80bb759c63b1b3564406356e2ef0b3712
        Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
        Date:   Mon Oct 23 17:08:48 2023 +0100
    
            feat: Add fruits
    
        commit a55d3b7bf4cb60df5502789f829442489583dd8f
        Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
        Date:   Mon Oct 23 17:08:08 2023 +0100
    
            feat: Add Snake movement and check if finished
    
        commit d464b870cecee1fb571fb7ece716720e4b2ebd64
        Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
        Date:   Mon Oct 23 17:06:40 2023 +0100
    
            feat: Add Snake class
    
        commit ca23d3a86f0be22d0b0145bcc1bf16ae73356bdd
        Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
        Date:   Mon Oct 23 17:05:34 2023 +0100
    
            feat: Add game logic
    
        commit b1eb359520d95cd736642d77c9141f12de88c932
        Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
        Date:   Mon Oct 23 17:04:08 2023 +0100
    
            feat: Add Game class
    
        commit 7f598eaf1f3c0d0dbe4843b6b85bf7e9716f0a96
        Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
        Date:   Mon Oct 23 17:00:28 2023 +0100
    
            feat: Add .gitignore

---
+++ Snake/Game.cs
@@ -0,0 +1,67 @@
+﻿namespace Snake {
+    public class Game {
+        public int width { get; private set; }
+        public int height { get; private set; }
+        private string[,] map;
+        public int[] fruit { get; private set; }
+        public int score { get; set; }
+
+        public Game(int height, int width) {
+            this.width = width;
+            this.height = height;
+            map = new string[width, height];
+            FillMap();
+        }
+
+        public void FillMap() {
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    map[0, j] = "■";
+                    map[i, j] = " ";
+                    map[width - 1, j] = "■";
+                }
+                map[i, 0] = "■";
+                map[i, height - 1] = "■";
+            }
+        }
+
+        public void Draw() {
+            for (int i = 0; i < height; i++) {
+                string line = "";
+                for (int j = 0; j < width; j++) {
+                    line += map[j, i] + " ";
+                }
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("Fruits eaten: " + score);
+        }
+
+        public void MoveSnake(Snake snake) {
+            map[snake.tail[0], snake.tail[1]] = " ";
+            for (int i = snake.size - 1; i > 0; i--) {
+                snake.body[i, 0] = snake.body[i - 1, 0];
+                snake.body[i, 1] = snake.body[i - 1, 1];
+            }
+            snake.body[0, 0] += snake.directionVector[0];
+            snake.body[0, 1] += snake.directionVector[1];
+            snake.tail[0] = snake.body[snake.size - 1, 0];
+            snake.tail[1] = snake.body[snake.size - 1, 1];
+            for (int i = 0; i < snake.size; i++) {
+                map[snake.body[i, 0], snake.body[i, 1]] = "▣";
+            }
+        }
+
+        public void SpawnFruit() {
+            while (true) {
+                Random random = new Random();
+                int x = random.Next(1, width - 1);
+                int y = random.Next(1, height - 1);
+                if (map[x, y] == " ") {
+                    map[x, y] = "◉";
+                    fruit = new int[] { x, y };
+                    break;
+                }
+            }
+        }
+    }
+}
