ec6196ba0a2f3db49875dbf3d3c4617ee25ca947
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-23 18:59:44 +0100

    Squashed commit of the following:
    commit 13df904091c9bf328503a98693db8797e1bd2cad
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 18:59:08 2023 +0100
    
        feat: Show score all the time. Also change layout
    
    commit a77845e724ab711ed251598e509096400a0bfd17
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:09:10 2023 +0100
    
        feat: Add timer
    
    commit 2ad0eeb80bb759c63b1b3564406356e2ef0b3712
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:08:48 2023 +0100
    
        feat: Add fruits
    
    commit a55d3b7bf4cb60df5502789f829442489583dd8f
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:08:08 2023 +0100
    
        feat: Add Snake movement and check if finished
    
    commit d464b870cecee1fb571fb7ece716720e4b2ebd64
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:06:40 2023 +0100
    
        feat: Add Snake class
    
    commit ca23d3a86f0be22d0b0145bcc1bf16ae73356bdd
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:05:34 2023 +0100
    
        feat: Add game logic
    
    commit b1eb359520d95cd736642d77c9141f12de88c932
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:04:08 2023 +0100
    
        feat: Add Game class
    
    commit 7f598eaf1f3c0d0dbe4843b6b85bf7e9716f0a96
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:00:28 2023 +0100
    
        feat: Add .gitignore

---
+++ Snake/Snake.cs
@@ -0,0 +1,82 @@
+﻿namespace Snake {
+    public class Snake {
+        public int size { get; private set; }
+        public int[,] body { get; private set; }
+        public int[] head { get; private set; }
+        public int[] tail { get; private set; }
+        private int direction;
+        public int[] directionVector { get; private set; }
+
+        public Snake(int size, int[] head, int direction) {
+            this.size = size;
+            this.head = head;
+            this.direction = direction;
+            directionVector = new int[2];
+            switch (direction) {
+                case 0:
+                    directionVector[0] = 0;
+                    directionVector[1] = -1;
+                    break;
+                case 1:
+                    directionVector[0] = 1;
+                    directionVector[1] = 0;
+                    break;
+                case 2:
+                    directionVector[0] = 0;
+                    directionVector[1] = 1;
+                    break;
+                case 3:
+                    directionVector[0] = -1;
+                    directionVector[1] = 0;
+                    break;
+            }
+            body = new int[size, 2];
+            for (int i = 0; i < size; i++) {
+                body[i, 0] = head[0] - i * directionVector[0];
+                body[i, 1] = head[1] - i * directionVector[1];
+            }
+            tail = new int[2];
+            tail[0] = body[size - 1, 0];
+            tail[1] = body[size - 1, 1];
+        }
+
+        public void ChangeDirection(int direction) {
+            if (this.direction == direction) return;
+            switch (direction) {
+                case 0: // Up
+                    if (this.direction == 2) return;
+                    directionVector[0] = 0;
+                    directionVector[1] = -1;
+                    break;
+                case 1: // Right
+                    if (this.direction == 3) return;
+                    directionVector[0] = 1;
+                    directionVector[1] = 0;
+                    break;
+                case 2: // Down
+                    if (this.direction == 0) return;
+                    directionVector[0] = 0;
+                    directionVector[1] = 1;
+                    break;
+                case 3: // Left
+                    if (this.direction == 1) return;
+                    directionVector[0] = -1;
+                    directionVector[1] = 0;
+                    break;
+            }
+            this.direction = direction;
+        }
+
+        public void Grow() {
+            size++;
+            int[,] newBody = new int[size, 2];
+            for (int i = 0; i < size - 1; i++) {
+                newBody[i, 0] = body[i, 0];
+                newBody[i, 1] = body[i, 1];
+            }
+            newBody[size - 1, 0] = tail[0];
+            newBody[size - 1, 1] = tail[1];
+            body = newBody;
+        }
+    }
+}
