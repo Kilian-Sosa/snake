60a15daefc6699e5c9bb592b9a88816287977438
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-27 19:08:27 +0100

    Squashed commit of the following:
    commit 5ea7e6bda4ad7accd4d4302137f3d01a138fd800
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Fri Oct 27 19:08:13 2023 +0100
    
        refactor: Change class properties to PascalCase
    
    commit d6e15264841366b9f77b135ab39da6701df6dd10
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Tue Oct 24 19:07:19 2023 +0100
    
        feat: Add README
    
    commit 698f31ce9fc45814ff2e0e845f8f207743d1bf11
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Tue Oct 24 18:35:05 2023 +0100
    
        fix: Now interval is decreased correctly
    
    commit 13df904091c9bf328503a98693db8797e1bd2cad
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 18:59:08 2023 +0100
    
        feat: Show score all the time. Also change layout
    
    commit a77845e724ab711ed251598e509096400a0bfd17
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:09:10 2023 +0100
    
        feat: Add timer
    
    commit 2ad0eeb80bb759c63b1b3564406356e2ef0b3712
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:08:48 2023 +0100
    
        feat: Add fruits
    
    commit a55d3b7bf4cb60df5502789f829442489583dd8f
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:08:08 2023 +0100
    
        feat: Add Snake movement and check if finished
    
    commit d464b870cecee1fb571fb7ece716720e4b2ebd64
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:06:40 2023 +0100
    
        feat: Add Snake class
    
    commit ca23d3a86f0be22d0b0145bcc1bf16ae73356bdd
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:05:34 2023 +0100
    
        feat: Add game logic
    
    commit b1eb359520d95cd736642d77c9141f12de88c932
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:04:08 2023 +0100
    
        feat: Add Game class
    
    commit 7f598eaf1f3c0d0dbe4843b6b85bf7e9716f0a96
    Author: Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
    Date:   Mon Oct 23 17:00:28 2023 +0100
    
        feat: Add .gitignore

--- Snake/Game.cs
+++ Snake/Game.cs
@@ -1,64 +1,65 @@
 ﻿namespace Snake {
     public class Game {
-        public int width { get; private set; }
-        public int height { get; private set; }
-        private string[,] map;
-        public int[] fruit { get; private set; }
-        public int score { get; set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        private string[,] Map;
+        public int[] Fruit { get; private set; }
+        public int Score { get; set; }
 
         public Game(int height, int width) {
-            this.width = width;
-            this.height = height;
-            map = new string[width, height];
+            this.Width = width;
+            this.Height = height;
+            Map = new string[width, Height];
             FillMap();
+            SpawnFruit();
         }
 
         public void FillMap() {
-            for (int i = 0; i < width; i++) {
-                for (int j = 0; j < height; j++) {
-                    map[0, j] = "■";
-                    map[i, j] = " ";
-                    map[width - 1, j] = "■";
+            for (int i = 0; i < Width; i++) {
+                for (int j = 0; j < Height; j++) {
+                    Map[0, j] = "■";
+                    Map[i, j] = " ";
+                    Map[Width - 1, j] = "■";
                 }
-                map[i, 0] = "■";
-                map[i, height - 1] = "■";
+                Map[i, 0] = "■";
+                Map[i, Height - 1] = "■";
             }
         }
 
         public void Draw() {
-            for (int i = 0; i < height; i++) {
+            for (int i = 0; i < Height; i++) {
                 string line = "";
-                for (int j = 0; j < width; j++) {
-                    line += map[j, i] + " ";
+                for (int j = 0; j < Width; j++) {
+                    line += Map[j, i] + " ";
                 }
                 Console.WriteLine(line);
             }
-            Console.WriteLine("Fruits eaten: " + score);
+            Console.WriteLine("Fruits eaten: " + Score);
         }
 
         public void MoveSnake(Snake snake) {
-            map[snake.tail[0], snake.tail[1]] = " ";
-            for (int i = snake.size - 1; i > 0; i--) {
-                snake.body[i, 0] = snake.body[i - 1, 0];
-                snake.body[i, 1] = snake.body[i - 1, 1];
+            Map[snake.Tail[0], snake.Tail[1]] = " ";
+            for (int i = snake.Size - 1; i > 0; i--) {
+                snake.Body[i, 0] = snake.Body[i - 1, 0];
+                snake.Body[i, 1] = snake.Body[i - 1, 1];
             }
-            snake.body[0, 0] += snake.directionVector[0];
-            snake.body[0, 1] += snake.directionVector[1];
-            snake.tail[0] = snake.body[snake.size - 1, 0];
-            snake.tail[1] = snake.body[snake.size - 1, 1];
-            for (int i = 0; i < snake.size; i++) {
-                map[snake.body[i, 0], snake.body[i, 1]] = "▣";
+            snake.Body[0, 0] += snake.DirectionVector[0];
+            snake.Body[0, 1] += snake.DirectionVector[1];
+            snake.Tail[0] = snake.Body[snake.Size - 1, 0];
+            snake.Tail[1] = snake.Body[snake.Size - 1, 1];
+            for (int i = 0; i < snake.Size; i++) {
+                Map[snake.Body[i, 0], snake.Body[i, 1]] = "▣";
             }
         }
 
         public void SpawnFruit() {
             while (true) {
                 Random random = new Random();
-                int x = random.Next(1, width - 1);
-                int y = random.Next(1, height - 1);
-                if (map[x, y] == " ") {
-                    map[x, y] = "◉";
-                    fruit = new int[] { x, y };
+                int x = random.Next(1, Width - 1);
+                int y = random.Next(1, Height - 1);
+                if (Map[x, y] == " ") {
+                    Map[x, y] = "◉";
+                    Fruit = new int[] { x, y };
                     break;
                 }
             }
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -1,82 +1,82 @@
 ﻿namespace Snake {
     public class Snake {
-        public int size { get; private set; }
-        public int[,] body { get; private set; }
-        public int[] head { get; private set; }
-        public int[] tail { get; private set; }
-        private int direction;
-        public int[] directionVector { get; private set; }
+        public int Size { get; private set; }
+        public int[,] Body { get; private set; }
+        public int[] Head { get; private set; }
+        public int[] Tail { get; private set; }
+        private int Direction;
+        public int[] DirectionVector { get; private set; }
 
         public Snake(int size, int[] head, int direction) {
-            this.size = size;
-            this.head = head;
-            this.direction = direction;
-            directionVector = new int[2];
-            switch (direction) {
+            this.Size = size;
+            this.Head = head;
+            this.Direction = direction;
+            DirectionVector = new int[2];
+            switch (Direction) {
                 case 0:
-                    directionVector[0] = 0;
-                    directionVector[1] = -1;
+                    DirectionVector[0] = 0;
+                    DirectionVector[1] = -1;
                     break;
                 case 1:
-                    directionVector[0] = 1;
-                    directionVector[1] = 0;
+                    DirectionVector[0] = 1;
+                    DirectionVector[1] = 0;
                     break;
                 case 2:
-                    directionVector[0] = 0;
-                    directionVector[1] = 1;
+                    DirectionVector[0] = 0;
+                    DirectionVector[1] = 1;
                     break;
                 case 3:
-                    directionVector[0] = -1;
-                    directionVector[1] = 0;
+                    DirectionVector[0] = -1;
+                    DirectionVector[1] = 0;
                     break;
             }
-            body = new int[size, 2];
+            Body = new int[size, 2];
             for (int i = 0; i < size; i++) {
-                body[i, 0] = head[0] - i * directionVector[0];
-                body[i, 1] = head[1] - i * directionVector[1];
+                Body[i, 0] = head[0] - i * DirectionVector[0];
+                Body[i, 1] = head[1] - i * DirectionVector[1];
             }
-            tail = new int[2];
-            tail[0] = body[size - 1, 0];
-            tail[1] = body[size - 1, 1];
+            Tail = new int[2];
+            Tail[0] = Body[size - 1, 0];
+            Tail[1] = Body[size - 1, 1];
         }
 
         public void ChangeDirection(int direction) {
-            if (this.direction == direction) return;
+            if (this.Direction == direction) return;
             switch (direction) {
                 case 0: // Up
-                    if (this.direction == 2) return;
-                    directionVector[0] = 0;
-                    directionVector[1] = -1;
+                    if (this.Direction == 2) return;
+                    DirectionVector[0] = 0;
+                    DirectionVector[1] = -1;
                     break;
                 case 1: // Right
-                    if (this.direction == 3) return;
-                    directionVector[0] = 1;
-                    directionVector[1] = 0;
+                    if (this.Direction == 3) return;
+                    DirectionVector[0] = 1;
+                    DirectionVector[1] = 0;
                     break;
                 case 2: // Down
-                    if (this.direction == 0) return;
-                    directionVector[0] = 0;
-                    directionVector[1] = 1;
+                    if (this.Direction == 0) return;
+                    DirectionVector[0] = 0;
+                    DirectionVector[1] = 1;
                     break;
                 case 3: // Left
-                    if (this.direction == 1) return;
-                    directionVector[0] = -1;
-                    directionVector[1] = 0;
+                    if (this.Direction == 1) return;
+                    DirectionVector[0] = -1;
+                    DirectionVector[1] = 0;
                     break;
             }
-            this.direction = direction;
+            this.Direction = direction;
         }
 
         public void Grow() {
-            size++;
-            int[,] newBody = new int[size, 2];
-            for (int i = 0; i < size - 1; i++) {
-                newBody[i, 0] = body[i, 0];
-                newBody[i, 1] = body[i, 1];
+            Size++;
+            int[,] newBody = new int[Size, 2];
+            for (int i = 0; i < Size - 1; i++) {
+                newBody[i, 0] = Body[i, 0];
+                newBody[i, 1] = Body[i, 1];
             }
-            newBody[size - 1, 0] = tail[0];
-            newBody[size - 1, 1] = tail[1];
-            body = newBody;
+            newBody[Size - 1, 0] = Tail[0];
+            newBody[Size - 1, 1] = Tail[1];
+            Body = newBody;
         }
     }
 }
