5ea7e6bda4ad7accd4d4302137f3d01a138fd800
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-27 19:08:13 +0100

    refactor: Change class properties to PascalCase

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -3,11 +3,11 @@ using Timer = System.Timers.Timer;
 
 namespace Snake {
     public class Program {
-        static Game game = new Game(28, 60);
-        static Snake snake = new Snake(3, new int[] { 3, 14 }, 1);
-        static Timer gameTimer;
-        static int initTimer = 800;
-        static int timerUpdate = 100;
+        static Game Game = new Game(28, 60);
+        static Snake Snake = new Snake(3, new int[] { 3, 14 }, 1);
+        static Timer GameTimer;
+        static int InitTimer = 600;
+        static int TimerUpdate = 100;
 
         public static void Main(string[] args) {
             Console.OutputEncoding = System.Text.Encoding.UTF8; // For the symbols to work
@@ -16,25 +16,24 @@ namespace Snake {
 
         public static void Run() {
             ConsoleKeyInfo keyInfo;
-            game.Draw();
-            game.SpawnFruit();
-            gameTimer = new Timer(initTimer); // One second interval
-            gameTimer.Elapsed += HandleTimerElapsed;
-            gameTimer.Start();
+            Game.Draw();
+            GameTimer = new Timer(InitTimer); // One second interval
+            GameTimer.Elapsed += HandleTimerElapsed;
+            GameTimer.Start();
             do {
                 keyInfo = Console.ReadKey(true);
                 switch (keyInfo.Key) {
                     case ConsoleKey.UpArrow:
-                        snake.ChangeDirection(0);
+                        Snake.ChangeDirection(0);
                         break;
                     case ConsoleKey.DownArrow:
-                        snake.ChangeDirection(2);
+                        Snake.ChangeDirection(2);
                         break;
                     case ConsoleKey.LeftArrow:
-                        snake.ChangeDirection(3);
+                        Snake.ChangeDirection(3);
                         break;
                     case ConsoleKey.RightArrow:
-                        snake.ChangeDirection(1);
+                        Snake.ChangeDirection(1);
                         break;
                 }
                 Eat();
@@ -42,28 +41,28 @@ namespace Snake {
         }
 
         public static bool HasLost() {
-            if (snake.body[0, 0] == 0 || snake.body[0, 0] == game.width - 1 || snake.body[0, 1] == 0 || snake.body[0, 1] == game.height - 1) return true;
-            for (int i = 1; i < snake.size; i++) 
-                if (snake.body[0, 0] == snake.body[i, 0] && snake.body[0, 1] == snake.body[i, 1]) return true;
+            if (Snake.Body[0, 0] == 0 || Snake.Body[0, 0] == Game.Width - 1 || Snake.Body[0, 1] == 0 || Snake.Body[0, 1] == Game.Height - 1) return true;
+            for (int i = 1; i < Snake.Size; i++) 
+                if (Snake.Body[0, 0] == Snake.Body[i, 0] && Snake.Body[0, 1] == Snake.Body[i, 1]) return true;
             return false;
         }
 
         public static void Eat() {
-            if (snake.body[0, 0] == game.fruit[0] && snake.body[0, 1] == game.fruit[1]) {
-                snake.Grow();
-                game.score++;
-                game.SpawnFruit();
-                if (game.score % 10 == 0 && gameTimer.Interval >= 400) gameTimer.Interval -= timerUpdate;
+            if (Snake.Body[0, 0] == Game.Fruit[0] && Snake.Body[0, 1] == Game.Fruit[1]) {
+                Snake.Grow();
+                Game.Score++;
+                Game.SpawnFruit();
+                if (Game.Score % 10 == 0 && GameTimer.Interval >= 200) GameTimer.Interval -= TimerUpdate;
             }
         }
 
         public static void HandleTimerElapsed(object sender, ElapsedEventArgs e) {
-            game.MoveSnake(snake);
+            Game.MoveSnake(Snake);
             Console.Clear();
-            game.Draw();
+            Game.Draw();
             if (HasLost()) {
                 Console.WriteLine("You lost!");
-                gameTimer.Stop();
+                GameTimer.Stop();
             }
             Eat();
         }
